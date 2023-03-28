2e9f66a9e940153b3a49e869fb9b698afdf03958
KieceDonc <valentinverst.developer@gmail.com>
2023-03-28 16:37:35 +0200

    almost finish iterative

--- projet/matrix.ml
+++ projet/matrix.ml
@@ -1,66 +1,66 @@
 module Matrix = struct
     let print matrix =
-    let matrixSize = Array.length matrix in
-    for x = 0 to matrixSize - 1 do
-        print_string "-------";
-    done;
-    print_string "\n";
-    for x = 0 to matrixSize - 1 do
-        for y = 0 to matrixSize - 1 do
-        print_float matrix.(x).(y);
-        print_string "\t";
+        let matrixSize = Array.length matrix in
+        for x = 0 to matrixSize - 1 do
+            print_string "-------";
         done;
         print_string "\n";
-    done;;
-
-    (* Fonction pour renvoyer la transposée d'une matrice *)
-    let transpose matrix =
-    let matrixSize = Array.length matrix in
-    let transposedMatrix = Array.make_matrix matrixSize matrixSize 0.0 in
-    for x = 0 to matrixSize - 1 do
-        for y = 0 to matrixSize - 1 do
-        transposedMatrix.(x).(y) <- matrix.(y).(x)
-        done;
-    done;
-    transposedMatrix
+        for x = 0 to matrixSize - 1 do
+            for y = 0 to matrixSize - 1 do
+            print_float matrix.(x).(y);
+            print_string "\t";
+            done;
+            print_string "\n";
+        done;;
 
     (* Crée la matrice identité *)
     let createIdentity matrixSize =
-    let identityMatrix = Array.make_matrix matrixSize matrixSize 0.0 in
-    for x = 0 to matrixSize - 1 do
-        identityMatrix.(x).(x) <- 1.0;
-    done;
-    identityMatrix;;
-
-    (* Transforme une matrice avec une taille étant une puissance de 2. Complète les nouvelles valeurs avec un 0 *)
-    let transform matrix = 
-    let inputSize = Array.length matrix in
-    let finalSize = int_of_float(2. ** ceil(log(float_of_int inputSize) /. log(2.0))) in (*Recherche la puissance de 2 supérieur la plus proche*)
-    
-    let finalMatrix = Array.make_matrix finalSize finalSize 0.0 in
-
-    for x = 0 to finalSize - 1 do
-        for y = 0 to finalSize - 1 do
-        if x < inputSize && y < inputSize then
-            finalMatrix.(x).(y) <- matrix.(x).(y)
-        else
-            finalMatrix.(x).(y) <- 0.0
-        done
-    done;
-
-    finalMatrix
+        let identityMatrix = Array.make_matrix matrixSize matrixSize 0.0 in
+        for x = 0 to matrixSize - 1 do
+            identityMatrix.(x).(x) <- 1.0;
+        done;
+        identityMatrix
 
     (* Crée une matrice contenant des valeurs de 0 à 9 avec une taille donnée *)
     let create size =
-    let matrix = Array.make_matrix size size 0.0 in
-    
-    for x = 0 to size - 1 do
-        for y = 0 to size - 1 do
-        matrix.(x).(y) <- float_of_int (Random.int 10)
-        done
-    done;
+        let matrix = Array.make_matrix size size 0.0 in
+        
+        for x = 0 to size - 1 do
+            for y = 0 to size - 1 do
+            matrix.(x).(y) <- float_of_int (Random.int 10)
+            done;
+        done;
 
-    matrix
+        matrix
+    
+    (* Fonction pour renvoyer la transposée d'une matrice *)
+    let transpose matrix =
+        let matrixSize = Array.length matrix in
+        let transposedMatrix = Array.make_matrix matrixSize matrixSize 0.0 in
+        for x = 0 to matrixSize - 1 do
+            for y = 0 to matrixSize - 1 do
+            transposedMatrix.(x).(y) <- matrix.(y).(x)
+            done;
+        done;
+        transposedMatrix
+
+    (* Transforme une matrice avec une taille étant une puissance de 2. Complète les nouvelles valeurs avec un 0 *)
+    let transform matrix = 
+        let inputSize = Array.length matrix in
+        let finalSize = int_of_float(2. ** ceil(log(float_of_int inputSize) /. log(2.0))) in (*Recherche la puissance de 2 supérieur la plus proche*)
+        
+        let finalMatrix = Array.make_matrix finalSize finalSize 0.0 in
+
+        for x = 0 to finalSize - 1 do
+            for y = 0 to finalSize - 1 do
+            if x < inputSize && y < inputSize then
+                finalMatrix.(x).(y) <- matrix.(x).(y)
+            else
+                finalMatrix.(x).(y) <- 0.0
+            done;
+        done;
+
+        finalMatrix
 
     (* 
     Crée une matrice de la forme A = B Ct  
@@ -69,49 +69,49 @@ module Matrix = struct
     Ct étant la transposé de C
     *)
     let createSpecial matrixSize =
-    let subMatrixSize = matrixSize / 2 in
-    let result = Array.make_matrix matrixSize matrixSize 0.0 in
-    let b = create subMatrixSize in
-    let c = create subMatrixSize in
-    let d = create subMatrixSize in
-    let ct = transpose c in
-    for x = 0 to subMatrixSize - 1 do
-        for y = 0 to subMatrixSize - 1 do
-        result.(x).(y) <- b.(x).(y);
-        result.(x).(y + subMatrixSize) <- ct.(x).(y);
-        result.(x + subMatrixSize).(y) <- c.(x).(y);
-        result.(x + subMatrixSize).(y + subMatrixSize) <- d.(x).(y)
+        let subMatrixSize = matrixSize / 2 in
+        let result = Array.make_matrix matrixSize matrixSize 0.0 in
+        let b = create subMatrixSize in
+        let c = create subMatrixSize in
+        let d = create subMatrixSize in
+        let ct = transpose c in
+        for x = 0 to subMatrixSize - 1 do
+            for y = 0 to subMatrixSize - 1 do
+            result.(x).(y) <- b.(x).(y);
+            result.(x).(y + subMatrixSize) <- ct.(x).(y);
+            result.(x + subMatrixSize).(y) <- c.(x).(y);
+            result.(x + subMatrixSize).(y + subMatrixSize) <- d.(x).(y)
+            done;
         done;
-    done;
-    transform result
+        transform result
 
     (* Fonction permettant d'extraire une sous-matrice d'une matrice donnée *)
     let sub matrix topLeftx topLefty length =
-    let sub = Array.make_matrix length length 0.0 in
-    for x = 0 to length - 1 do
-        for y = 0 to length - 1 do
-        sub.(x).(y) <- matrix.(topLefty+x).(topLeftx+y)
-        done
-    done;
-    sub
+        let sub = Array.make_matrix length length 0.0 in
+        for x = 0 to length - 1 do
+            for y = 0 to length - 1 do
+                sub.(x).(y) <- matrix.(topLefty+x).(topLeftx+y)
+            done;
+        done;
+        sub
 
     (* Divise une matrice en quatre sous-matrices *)
     let split matrix =
-    let matrixSize = Array.length matrix in
-    let subMatrixSize = matrixSize / 2 in
-    let a11 = sub matrix 0 0 subMatrixSize in
-    let a12 = sub matrix subMatrixSize 0 subMatrixSize in
-    let a21 = sub matrix 0 subMatrixSize subMatrixSize in
-    let a22 = sub matrix subMatrixSize subMatrixSize subMatrixSize in
-    a11, a12, a21, a22
+        let matrixSize = Array.length matrix in
+        let subMatrixSize = matrixSize / 2 in
+        let a11 = sub matrix 0 0 subMatrixSize in
+        let a12 = sub matrix subMatrixSize 0 subMatrixSize in
+        let a21 = sub matrix 0 subMatrixSize subMatrixSize in
+        let a22 = sub matrix subMatrixSize subMatrixSize subMatrixSize in
+        a11, a12, a21, a22
 
     let multipleByConstant matrix constant = 
-    let matrixSize = Array.length matrix in
-    let result = Array.make_matrix matrixSize matrixSize 0.0 in
-    for x = 0 to matrixSize - 1 do
-        for y = 0 to matrixSize - 1 do
-        result.(x).(y) <- (matrix.(x).(y) *. constant)
+        let matrixSize = Array.length matrix in
+        let result = Array.make_matrix matrixSize matrixSize 0.0 in
+        for x = 0 to matrixSize - 1 do
+            for y = 0 to matrixSize - 1 do
+                result.(x).(y) <- (matrix.(x).(y) *. constant)
+            done;
         done;
-    done;
-    result
+        result
 end
--- projet/projet.ml
+++ projet/projet.ml
@@ -1,62 +1,25 @@
-(*
-(* Produit matriciel avec les opérations de Strassen *)
-let rec strassenMultipleMatrices firstMatrix secondMatrix =
-  let matrixSize = Array.length firstMatrix in
-  if matrixSize = 1 then
-    let result = Array.make_matrix 1 1 0.0 in
-    result.(0).(0) <- firstMatrix.(0).(0) *. secondMatrix.(0).(0);
-    result
-  else
-    let a11, a12, a21, a22 = splitMatrix firstMatrix in
-    let b11, b12, b21, b22 = splitMatrix secondMatrix in
-    let s1 = strassenMultipleMatrices (addMatrices a11 a22) (addMatrices b11 b22) in
-    let s2 = strassenMultipleMatrices (addMatrices a21 a22) b11 in
-    let s3 = strassenMultipleMatrices a11 (subtractMatrices b12 b22) in
-    let s4 = strassenMultipleMatrices a22 (subtractMatrices b21 b11) in
-    let s5 = strassenMultipleMatrices (addMatrices a11 a12) b22 in
-    let s6 = strassenMultipleMatrices (subtractMatrices a21 a11) (addMatrices b11 b12) in
-    let s7 = strassenMultipleMatrices (subtractMatrices a12 a22) (addMatrices b21 b22) in
-    let c11 = addMatrices (subtractMatrices (addMatrices s1 s4) s5) s7 in
-    let c12 = addMatrices s3 s5 in
-    let c21 = addMatrices s2 s4 in
-    let c22 = addMatrices (subtractMatrices (addMatrices s1 s3) s2) s6 in
-    mergeMatrices c11 c12 c21 c22
+open Matrix
+open Matrices
 
-(* Retourne l'inverse d'une matrice *)
-(* https://sd.blackball.lv/library/Introduction_to_Algorithms_Third_Edition_(2009).pdf *)
-(* Page 830 *)
-(* https://www.sciencedirect.com/science/article/pii/S0377042708006237#b5 *)
-let rec inverseMatrix matrix =
-  let matrixSize = Array.length matrix in
-  if matrixSize = 1 then
-    let result = Array.make_matrix 1 1 0.0 in
-    if matrix.(0).(0) = 0 then
-      result.(0).(0) <- 0.0
-    else
-      result.(0).(0) <- 1.0 /. float_of_int(matrix.(0).(0));
-    result
-  else
-    let a11, a12, a21, a22 = splitMatrix matrix in
-    let r1 = inverseMatrix a11 in
-    let r2 = strassenMultipleMatrices a21 r1 in
-    let r3 = strassenMultipleMatrices r1 a12 in
-    let r4 = strassenMultipleMatrices a21 r3 in
-    let r5 = subtractMatrices r4 a22 in
-    let r6 = inverseMatrix r5 in
-    let x12 = strassenMultipleMatrices r3 r6 in
-    let x21 = strassenMultipleMatrices r6 r2 in
-    let r7 = strassenMultipleMatrices r3 x21 in
-    let x11 = subtractMatrices r1 r7 in
-    let x22 = multipleMatrixByConstant r6 (-1.) in
-    mergeMatrices x11 x12 x21 x22
-  
-let myFirstMatrix = transformMatrix(createMatrix(4));;
+let mySpecialMatrix = Matrix.createSpecial 4;;
+Matrix.print mySpecialMatrix;;
 
-let mySecondMatrix = transformMatrix(createMatrix(4));;
+let myInvertMatrix = Matrices.inverse mySpecialMatrix;;
+Matrix.print myInvertMatrix;;
 
-let myFirstSpecialMatrix = createSpecialMatrix 4;;
-printMatrix myFirstSpecialMatrix;;
+let myFirstMatrix = Matrix.transform(Matrix.create(256));;
 
-let myInvertMatrix = inverseMatrix myFirstSpecialMatrix;;
-printMatrix myInvertMatrix;;
-*)
+let mySecondMatrix = Matrix.transform(Matrix.create(256));;
+
+let t1 = Sys.time();; 
+Printf.printf "Début multiplication strassen\n";;
+let resultStrassen = Matrices.strassenMultiple myFirstMatrix mySecondMatrix;;
+let t2 = Sys.time();;
+Printf.printf "Fin multiplication strassen\nTemps d'exécution : %.2f secondes\n" (t2 -. t1);;
+
+Printf.printf "\n"
+let t1 = Sys.time();;
+Printf.printf "Début multiplication normal\n";;
+let resultNormal = Matrices.multiple myFirstMatrix mySecondMatrix;;
+let t2 = Sys.time();;
+Printf.printf "Fin multiplication normal\nTemps d'exécution : %.2f secondes\n" (t2 -. t1);;
