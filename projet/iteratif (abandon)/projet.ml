cd98eadbb8b7acbd8d37441f5fbe33242b8c1722
KieceDonc <valentinverst.developer@gmail.com>
2023-04-03 16:09:49 +0200

    small fixs

--- projet/iteratif (abandon)/projet.ml
+++ projet/iteratif (abandon)/projet.ml
@@ -1,11 +1,25 @@
 open Matrix
 open Matrices
 
-let mySpecialMatrix = Matrix.createSpecial 512;;
+let mySpecialMatrix = Matrix.createSpecial 4;;
 Matrix.print mySpecialMatrix;;
 
 let myInvertMatrix = Matrices.inverse mySpecialMatrix;;
 Matrix.print myInvertMatrix;;
 
-let test = Matrices.multiple mySpecialMatrix myInvertMatrix;;
-Matrix.print test
+let myFirstMatrix = Matrix.transform(Matrix.create(256));;
+
+let mySecondMatrix = Matrix.transform(Matrix.create(256));;
+
+let t1 = Sys.time();; 
+Printf.printf "Début multiplication strassen\n";;
+let resultStrassen = Matrices.strassenMultiple myFirstMatrix mySecondMatrix;;
+let t2 = Sys.time();;
+Printf.printf "Fin multiplication strassen\nTemps d'exécution : %.2f secondes\n" (t2 -. t1);;
+
+Printf.printf "\n"
+let t1 = Sys.time();;
+Printf.printf "Début multiplication normal\n";;
+let resultNormal = Matrices.multiple myFirstMatrix mySecondMatrix;;
+let t2 = Sys.time();;
+Printf.printf "Fin multiplication normal\nTemps d'exécution : %.2f secondes\n" (t2 -. t1);;
