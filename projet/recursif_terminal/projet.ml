821b63a7aac5ae4359e81b788f050f166d2f50f1
KieceDonc <valentin.verstracte@gmail.com>
2023-04-16 21:54:34 +0200

    End project

--- projet/recursif_terminal/projet.ml
+++ projet/recursif_terminal/projet.ml
@@ -1,9 +1,11 @@
 open Matrix
 open Matrices
 
-let myFirstMatrix = Matrix.transform(Matrix.create(2));;
+let matrixSize = 32;;
 
-let mySecondMatrix = Matrix.transform(Matrix.create(2));;
+let myFirstMatrix = Matrix.transform(Matrix.create(matrixSize));;
+
+let mySecondMatrix = Matrix.transform(Matrix.create(matrixSize));;
 
 let t1 = Sys.time();; 
 Printf.printf "Début multiplication strassen\n";;
@@ -18,8 +20,7 @@ let resultNormal = Matrices.multiple myFirstMatrix mySecondMatrix;;
 let t2 = Sys.time();;
 Printf.printf "Fin multiplication normal\nTemps d'exécution : %.2f secondes\n" (t2 -. t1);;
 
-let specialMatrixSize = 4;;
-let mySpecialMatrix = Matrix.createSpecial specialMatrixSize;;
+let mySpecialMatrix = Matrix.createSpecial matrixSize;;
 Matrix.print mySpecialMatrix;;
 
 let myInverseMatrix = Matrices.inverse mySpecialMatrix;;
@@ -28,11 +29,19 @@ Matrix.print myInverseMatrix;;
 let approxIdentityMatrix = Matrices.multiple mySpecialMatrix myInverseMatrix;;
 Matrix.print approxIdentityMatrix;;
 
-let identityMatrix = Matrix.createIdentity specialMatrixSize;;
+let identityMatrix = Matrix.createIdentity matrixSize;;
 
 let diffMatrix = Matrices.subtract approxIdentityMatrix identityMatrix;;
 let totalDiff = Array.fold_left
   (fun acc (x,y) -> acc +. abs_float (diffMatrix.(x).(y)))
   0.0
-  (Array.init (specialMatrixSize * specialMatrixSize) (fun x -> (x / specialMatrixSize, x mod specialMatrixSize)));;
+  (Array.init (matrixSize * matrixSize) (fun x -> (x / matrixSize, x mod matrixSize)));;
 print_float totalDiff;;
+Printf.printf "\n";;
+
+let totalDiff = Array.fold_left
+  (fun acc (x,y) -> acc +. diffMatrix.(x).(y))
+  0.0
+  (Array.init (matrixSize * matrixSize) (fun x -> (x / matrixSize, x mod matrixSize)));;
+print_float totalDiff;;
+Printf.printf "\n";;
